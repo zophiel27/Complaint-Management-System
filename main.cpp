3e26fecb0fcda4e6b76a4556515bbe2d8ca23394
Rafay Chohan <abrkchohan@gmail.com>
2023-11-28 01:52:07 +0500

    Update main.cpp
    Added Core classes and their core functions(only definitions)

--- main.cpp
+++ main.cpp
@@ -5,155 +5,99 @@
 #include <fstream>
 
 using namespace std;
-// Complaint data structure
+
+//Class for main system
+class System{
+    private:
+    public:
+};
+
+// Complaint Class
 class Complaint {
+    private:
 	int id; // Complaint ID
 	string description; // Complaint description
-	string teacher; // Teacher who filed the complaint
-	string department; // Department assigned to
-	string status; // Complaint status (new, assigned, resolved, closed)
+	Teacher *teacher; // Teacher who filed the complaint
+	Department *department; // Department assigned to
+	string status; // Complaint status (new, assigned, resolved or closed. The teacher or manager can view its status any time)
 	string dateFiled; // Date complaint was filed
 	string dateAssigned; // Date complaint was assigned to an employee
 	string dateResolved; // Date complaint was marked as resolved
 	string dateClosed; // Date complaint was closed
-	vector<string> assignedEmployees; // Employees assigned to the complaint
+	//vector<string> assignedEmployees; // Employees assigned to the complaint
 	string feedback; // Teacher's feedback
+    public:
 };
 
-// Employee data structure
+//Department Class
+class Department{
+    private:
+    string name; //IT, accounts or admin.
+    vector<Employee *> employees;
+    Employee manager;
+    vector<Complaint *> tasks;
+    vector<Complaint *> assignedtasks;
+    vector<Complaint *> completedtasks;
+    public:
+    void pushforReview();
+    void notifyteacher();
+    void assignTask();
+    void completeTask();
+
+
+};
+
+// Employee Class INT A manager, INT B employee
 class Employee {
+    private:
 	int id; // Employee ID
 	string name; // Employee name
 	string department; // Employee's department
+    vector<string> tasks;
+    public:
+    //Employee
+    void checkTasks();
+    void completeTask();
+    void updateSystem();
+    //manager
+    void reviewTask();
+    void reviewRequest();
+    void assignTask();
+    void checkComplainProgress();
+    void notifySystem();
 };
 
-// Manager data structure
-class Manager {
-	int id; // Manager ID
-	string name; // Manager name
-	string department; // Manager's department
-};
-
-// Teacher data structure
+// Teacher Class
 class Teacher {
+    private:
 	int id; // Teacher ID
 	string name; // Teacher name
+    vector<string> notifications;
+    public:
+    void fileComplaint();//if there is any problem, or if any service/equipment is required, teacher shall forward her request to a particular supporting department such as IT, accounts or admin.
+    void checkComplainProgress();
+    void checkNotifications();
+    void printNotifications();
+    void clearNotifications();
+    void recordFeedback();
+
+
 };
-// Function to read complaints from file
-vector<Complaint> readComplaints() {
-    vector<Complaint> complaints;
-    ifstream file("complaints.dat");
-    if (file.is_open()) {
-        string line;
-        while (getline(file, line)) {
-            Complaint complaint;
-            // Parse the line to extract complaint details
-            // ...
-            complaints.push_back(complaint);
-        }
-        file.close();
-    }
-    return complaints;
-}
 
-// Function to write complaints to file
-void writeComplaints(vector<Complaint> complaints) {
-    ofstream file("complaints.dat");
-    if (file.is_open()) {
-        for (const Complaint& complaint : complaints) {
-            // Write complaint details to file
-            // ...
-        }
-        file.close();
-    }
-}
+//Admin Class, can be singelton
+class Admin{
+    private:
+	int id; // Admin ID
+	string name; // Admin name
+    public:
+    void addEmployee();
+    void addManager();
+    void addTeacher();
+    void removeEmployee();
+    void removeManager();
+    void removeTeacher();
+};
 
-// Function to read employees from file
-vector<Employee> readEmployees() {
-    vector<Employee> employees;
-    ifstream file("employees.dat");
-    if (file.is_open()) {
-        string line;
-        while (getline(file, line)) {
-            Employee employee;
-            // Parse the line to extract employee details
-            // ...
-            employees.push_back(employee);
-        }
-        file.close();
-    }
-    return employees;
-}
-
-// Function to write employees to file
-void writeEmployees(vector<Employee> employees) {
-    ofstream file("employees.dat");
-    if (file.is_open()) {
-        for (const Employee& employee : employees) {
-            // Write employee details to file
-            // ...
-        }
-        file.close();
-    }
-}
-
-// Function to read managers from file
-vector<Manager> readManagers() {
-    vector<Manager> managers;
-    ifstream file("managers.dat");
-    if (file.is_open()) {
-        string line;
-        while (getline(file, line)) {
-            Manager manager;
-            // Parse the line to extract manager details
-            // ...
-            managers.push_back(manager);
-        }
-        file.close();
-    }
-    return managers;
-}
-
-// Function to write managers to file
-void writeManagers(vector<Manager> managers) {
-    ofstream file("managers.dat");
-    if (file.is_open()) {
-        for (const Manager& manager : managers) {
-            // Write manager details to file
-            // ...
-        }
-        file.close();
-    }
-}
-
-// Function to read teachers from file
-vector<Teacher> readTeachers() {
-    vector<Teacher> teachers;
-    ifstream file("teachers.dat");
-    if (file.is_open()) {
-        string line;
-        while (getline(file, line)) {
-            Teacher teacher;
-            // Parse the line to extract teacher details
-            // ...
-            teachers.push_back(teacher);
-        }
-        file.close();
-    }
-    return teachers;
-}
-
-// Function to write teachers to file
-void writeTeachers(vector<Teacher> teachers) {
-    ofstream file("teachers.dat");
-    if (file.is_open()) {
-        for (const Teacher& teacher : teachers) {
-            // Write teacher details to file
-            // ...
-        }
-        file.close();
-    }
-}
 int main()
 {
     return 0;
